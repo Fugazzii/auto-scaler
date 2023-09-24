62646e6c1190fd4bc62c3f038ee9bac86d21b604
Fugazzii <sichinavailia@gmail.com>
2023-09-24 16:12:32 +0400

    Fixes

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,10 +2,12 @@ import { ProcessManager } from "./process";
 
 function main() {
 
-    const manager = ProcessManager.create();
+    const manager = ProcessManager.create(1, 5);
 
     manager.addProcess({ host: "localhost", port: "3000" });
     manager.addProcess({ host: "localhost", port: "3001" });
+
+    manager.killProcess();
 
     console.log("Running...");
 }
--- app/src/process/process-manager.ts
+++ app/src/process/process-manager.ts
@@ -5,27 +5,27 @@ type ChildProcess = Subprocess<"pipe", "pipe", "inherit">;
 
 export class ProcessManager {
     
-    private static minProcessCount = 1;
-    private static maxProcessCount = 5;
     private readonly processes: Array<ChildProcess>;
 
-    private constructor() {
+    private constructor(
+        private minProcessCount = 1,
+        private maxProcessCount = 5    
+    ) {
         this.processes = [];
     }
 
-    public static create() {
-        return new ProcessManager();
+    public static create(minProc: number, maxProc: number) {
+        return new ProcessManager(minProc, maxProc);
     }
 
     public addProcess({ host, port } : ProcessParams) {
 
-        if(this.processes.length === ProcessManager.maxProcessCount) {
-            console.log(`You can not run more than ${ProcessManager.maxProcessCount} processes`);
+        if(this.processes.length === this.maxProcessCount) {
+            console.log(`You can not run more than ${this.maxProcessCount} processes`);
             return;
         }
 
         const path = "/home/ilia/Desktop/Node/load-balancer/examples/simple-server/target/debug/simple-server";
-        const args = [];
 
         let process = spawn([path, host, port], {
             stdin: "pipe",
@@ -36,12 +36,14 @@ export class ProcessManager {
     }
 
     public killProcess() {
-        if(this.processes.length <= ProcessManager.minProcessCount) {
-            console.log(`You can not run less than ${ProcessManager.minProcessCount} processes`);
+        if(this.processes.length === this.minProcessCount) {
+            console.log(`You can not run less than ${this.minProcessCount} processes`);
             return;
         }
 
         let lastProcess = this.processes.pop() as ChildProcess;
+        lastProcess.stdin.flush();
+        lastProcess.stdin.end();
         lastProcess.kill();
     }
 
