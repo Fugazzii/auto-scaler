a41357dd6f429ed2e8035127b697384a2d062c95
Fugazzii <sichinavailia@gmail.com>
2023-09-24 12:24:25 +0400

    Initial

---
+++ examples/simple-server/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/ping")]
+async fn greet() -> impl Responder {
+    format!("pong!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
